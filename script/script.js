67aa7b9c108e60c95fc8de96de936299d1561a64
Pavel <pashakobozev@gmail.com>
2021-11-29 19:02:54 +0500

    fixed bass button

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
 
+
 function playLoop(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // choosing audio [element = event] 
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    
     audio.play();
     audio.currentTime = 0;
     key.classList.add('playing');
+
 };
+
+
 
 
 
@@ -20,4 +23,4 @@ window.addEventListener('keydown', playLoop);
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-keys.forEach(key => key.addEventListener('transitionend', playLoop));
+
